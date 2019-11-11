5c4a6544bd8ed34576d8e6a90bbfabcade3c8e9a
jorgelisapa <jorgelisapa@gmail.com>
2019-11-11 11:21:41 -0500

    Modified inputsin user form

--- src/frontend/components/UserDataForm.jsx
+++ src/frontend/components/UserDataForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import TextInput from './TextInput'
+import { TextInput } from './TextInput'
 import { Button } from './Button'
 
 import styles from '../styles/components/UserDataForm.module.scss'
@@ -10,21 +10,51 @@ export const UserDataForm = () => (
             <h2>Deseas modificar tus datos?</h2>
         </div>
         <form className={styles['form']} action=''>
-            <input className={styles['input']} type='text' placeholder='&#128100;  Nombre' required/>
-            <input className={styles['input']} type='text' placeholder='&#128100;  Apellido' required/>
 
-            <input className={styles['input']} type='email' placeholder='&#9993;  E-mail' required/>
+            <TextInput
+                className={styles['input']}
+                placeholder='&#128100; Nombre'
+                required
+            />
+            <TextInput
+                className={styles['input']}
+                placeholder='&#128100; Apellido'
+                required
+            />
+
+            <TextInput
+                className={styles['input']}
+                type='email'
+                placeholder='&#9993;  E-mail'
+                required
+            />
 
             <div className={styles['form__title']}>
                 <h3>Tambien puedes cambiar tu contraseña</h3>
             </div>
-            <input className={styles['input']} type='password' placeholder='&#9919;  Contraseña Actual' required/>
-            <input className={styles['input']} type='password' placeholder='&#9919;  Nueva Contraseña' required/>
-            <input className={styles['input']} type='password' placeholder='&#9919;  Confirmación Contraseña' required/>
+            <TextInput
+                className={styles['input']}
+                type='password'
+                placeholder='&#9919;  Contraseña Actual'
+                required
+            />
+            <TextInput
+                className={styles['input']}
+                type='password'
+                placeholder='&#9919;  Nueva Contraseña'
+                required
+            />
+            <TextInput
+                className={styles['input']}
+                type='password'
+                placeholder='&#9919;  Confirmación Contraseña'
+                required
+            />
             <div className='btn__form'>
                 <Button className={styles['btn__submit']}>
                     Guardar Cambios
                 </Button>
+
             </div>
         </form>
     </div>

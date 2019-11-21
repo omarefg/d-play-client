9474f0fb54c58c6617723acba26b5cad3acc7f86
jorgelisapa <jorgelisapa@gmail.com>
2019-11-21 14:05:25 -0500

    Header responsive

--- src/frontend/components/Header.jsx
+++ src/frontend/components/Header.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import { Search } from './Search'
 import { ListItem } from './ListItem'
 import DplayLogo from '../assets/Logo.png'
+import { ResponsiveIcon } from '../icons/ResponsiveIcon'
 
 import styles from '../styles/components/Header.module.scss'
 import userIcon from '../assets/Containers/Register/user-icon.png'
@@ -23,31 +25,36 @@ export const Header = withRouter(connect(mapStateToProps)(props => {
             <header className={styles['header__container']}>
                 <Link to='/'>
                     <img
+                        className={styles['header__logo']}
                         src={DplayLogo}
                         alt='logo'
                     />
                 </Link>
 
                 <div className={styles['header__childs-container']}>
-                    <Search/>
-                    <div className={styles['header__flex-grow']}/>
-                    <ul>
-                        <ListItem
-                            title='Recomendaciones'
-                            to='/recomendaciones'
-                            active={pathname === '/recomendaciones'}
-                        />
-                        <ListItem
-                            title='Mis listas'
-                            to='/mis-listas'
-                            active={pathname === '/mis-listas'}
-                        />
-                        <ListItem
-                            title='Géneros'
-                            to='/generos'
-                            active={pathname === '/generos'}
-                        />
-                    </ul>
+                    <div className={styles['header__search']}>
+                        <Search/>
+                    </div>
+
+                    <div className={styles['header__flex-grow']}>
+                        <ul>
+                            <ListItem
+                                title='Recomendaciones'
+                                to='/recomendaciones'
+                                active={pathname === '/recomendaciones'}
+                            />
+                            <ListItem
+                                title='Mis listas'
+                                to='/mis-listas'
+                                active={pathname === '/mis-listas'}
+                            />
+                            <ListItem
+                                title='Géneros'
+                                to='/generos'
+                                active={pathname === '/generos'}
+                            />
+                        </ul>
+                    </div>
                 </div>
                 <Link to='/perfil-usuario'>
                     <img
